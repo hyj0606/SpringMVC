efffb823d05a9225cb5d4a75913962db4d59d9e8
hyj0606 <1841406870@qq.com>
2022-11-18 14:46:47 +0800

    第一天

--- demo1/src/main/java/com/config/MyWebXml.java
+++ demo1/src/main/java/com/config/MyWebXml.java
@@ -25,23 +25,22 @@ public class MyWebXml extends AbstractAnnotationConfigDispatcherServletInitializ
     }
 
     /*
-    * /* : 拦截所有请求. 含:控制器方法 + 网页视图(jsp) + 静态资源(html,css,js,)
-    * /  : 拦截部分请求. 含: 控制器方法 + 静态资源
-    *
+    * /* : 拦截所有请求. 含:控制器方法 + 网页视图(jsp) + 静态资源(html,css,js,img)
+    * /  : 拦截部分请求. 含: 控制器方法 + 静态资源(放开: 静态资源访问配置)
+    * *.do : 拦截后缀为.do的请求. 注:控制器方法后缀名必须为声明为.do 例如: /t1.do
     * */
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
 
+    //扩展过滤器实现中文参数编码指定
     @Override
     protected Filter[] getServletFilters() {
-
         //通过返回过滤器进行生效:
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("utf-8");
 
         return new Filter[]{filter};
-
     }
 }
