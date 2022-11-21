77695c30df5d0cc4894e27cad89ec5f52dabd708
hyj0606 <1841406870@qq.com>
2022-11-21 09:50:42 +0800

    第一天

--- demo2/src/main/java/com/config/SpringMvcConfig.java
+++ demo2/src/main/java/com/config/SpringMvcConfig.java
@@ -1,8 +1,10 @@
 package com.config;
 
+import com.utils.MyInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
@@ -18,6 +20,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @EnableWebMvc //自定义配置
 public class SpringMvcConfig extends WebMvcConfigurationSupport {
 
+    //自定义springMVC容器配置工具:
 
 
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+
+        //自定义拦截器:
+        MyInterceptor my1 = new MyInterceptor();
+
+        //注册安装拦截器:
+        registry.addInterceptor(my1).addPathPatterns("/front/*");
+
+    }
 }
