3983d1a5fd2ca2d2f84de2174625a5525f2f34c5
hyj0606 <1841406870@qq.com>
2022-11-21 10:53:29 +0800

    第一天

--- demo2/src/main/java/com/config/SpringMvcConfig.java
+++ demo2/src/main/java/com/config/SpringMvcConfig.java
@@ -3,9 +3,8 @@ package com.config;
 import com.utils.MyInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * @ClassName SpringMvcConfig
@@ -22,7 +21,6 @@ public class SpringMvcConfig extends WebMvcConfigurationSupport {
 
     //自定义springMVC容器配置工具:
 
-
     //拦截器工具
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
@@ -32,6 +30,32 @@ public class SpringMvcConfig extends WebMvcConfigurationSupport {
 
         //注册安装拦截器:
         registry.addInterceptor(my1).addPathPatterns("/*/*");
+    }
+
+    //提供静态资源的访问配置:
+
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        //提供静态资源的访问配置:
+        registry.addResourceHandler("/common/**").addResourceLocations("/common/");
+        registry.addResourceHandler("/common/imgs/**").addResourceLocations("/common/imgs/");
+        registry.addResourceHandler("/common/css/**").addResourceLocations("/common/css/");
+        registry.addResourceHandler("/common/js/**").addResourceLocations("/common/js/");
+
+    }
+
+    //视图解析器: 根据提供的视图名称,搜索解析
+    @Override
+    protected void configureViewResolvers(ViewResolverRegistry registry) {
+
+        //创建
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+
+        registry.viewResolver(resolver);
 
     }
 }
