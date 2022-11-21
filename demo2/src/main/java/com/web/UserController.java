67511f36be88a0d581d0cf984e29eb33eb05bc49
hyj0606 <1841406870@qq.com>
2022-11-21 09:50:57 +0800

    第一天

---
+++ demo2/src/main/java/com/web/UserController.java
@@ -0,0 +1,29 @@
+package com.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName UserController
+ * @Description TODO
+ * @Author hyj98
+ * @Date 2022-11-21 9:48
+ * @Version 1.0
+ */
+@RestController
+public class UserController {
+
+    @RequestMapping("/front/index")
+    public String t1(){
+        System.out.println("欢迎来到前台");
+        return "ok";
+    }
+
+    @RequestMapping("/admin/index")
+    public String t2(){
+        System.out.println("欢迎来到后台");
+        return "ok";
+    }
+
+}
